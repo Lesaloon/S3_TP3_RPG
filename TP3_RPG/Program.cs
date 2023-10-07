8644b94eeea3ba979476426811121c4d7893e111
Lesaloon <47109666+Lesaloon@users.noreply.github.com>
2023-10-07 09:13:38 +0200

    testing the code

--- TP3_RPG/Program.cs
+++ TP3_RPG/Program.cs
@@ -1,13 +1,22 @@
-﻿namespace TP3_RPG
+﻿using TP3_RPG.Interface;
+
+namespace TP3_RPG
 {
 	class Program
 	{
 		static void Main(string[] args) {
-			string name = "unknown";
-			if (args.Length > 0) {
-				name = args[0];
+			// string name = "unknown";
+			// if (args.Length > 0) {
+			// 	name = args[0];
+			// }
+			// Console.WriteLine("Hi Mom ! It's me " + name);
+			Player player = new Player("Player");
+			Enemy enemy = new Enemy("Enemy");
+
+			while (player.IsAlive() && enemy.IsAlive()) {
+				player.Attack(enemy);
+				enemy.Attack(player);
 			}
-			Console.WriteLine("Hi Mom ! It's me " + name);
 		}
 	}
 } 
